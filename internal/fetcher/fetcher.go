e22d1e3e926a5407113eaeab66bdac439f3e65cd
Aleksei Yuzhanin <zombehalk@gmail.com>
2024-07-24 19:11:45 +0300

    commit

--- internal/fetcher/fetcher.go
+++ internal/fetcher/fetcher.go
@@ -2,7 +2,9 @@ package fetcher
 
 import (
 	"app/internal/storage/model"
+	"app/internal/source"
 	"context"
+	"sync"
 	"time"
 )
 
@@ -11,7 +13,13 @@ type ArticleStorage interface{
 }
 
 type SourseProvider interface {
-	Store(ctx context.Context) ([]model.Source, error)
+	Sources(ctx context.Context) ([]model.Source, error)
+}
+
+type Source interface{
+	ID() int64
+	Name() string
+	Fetch(ctx context.Context) ([]model.Item, error)
 }
 
 type Fetcher struct {
@@ -20,4 +28,36 @@ type Fetcher struct {
 
 	fetchInterval time.Duration
 	filteredKeywords []string
+}
+
+func New(
+	articleStorage ArticleStorage,
+	sourceProvider SourseProvider,
+	fetchInterval time.Duration,
+	filteredKeywords []string,
+) *Fetcher{
+	return &Fetcher{
+		articles: articleStorage,
+		sources: sourceProvider,
+		fetchInterval: fetchInterval,
+		filteredKeywords: filteredKeywords,
+	}
+}
+
+func (f *Fetcher) Fetch(ctx context.Context) error {
+	sources, err := f.sources.Sources(ctx)
+	if err != nil{
+		return err
+	}
+	var wg sync.WaitGroup
+
+	for _, src := range sources{
+		wg.Add(1)
+
+		rssSource := source.NewRSSSourceFromModel(src)
+
+		go func(source Source){
+			
+		}(src)
+	}
 }
